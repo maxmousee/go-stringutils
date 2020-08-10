50d7f18ed0411cf4216a5ea38c79b77cec1768db
Natan Facchin <maxmousee@gmail.com>
2020-08-10 17:30:26 +0200

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+package stringutils
+
+// stringutils contains common utility functions available in other programming languages and frameworks
+// to make it easier for you to manipulate strings, create a parser or a lexer in your project.
